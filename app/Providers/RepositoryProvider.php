b1c9011e35f1b3b0aa2b36e8ab1182da5b3c3e01
Mojtaba Rakhisi <m.rakhisi@liateam.net>
2022-03-13 22:03:43 +0330

    feat: register repositories

--- app/Providers/RepositoryProvider.php
+++ app/Providers/RepositoryProvider.php
@@ -2,9 +2,13 @@
 
 namespace App\Providers;
 
+use App\Repositories\Api\V1\CommentRepository;
+use App\Repositories\Api\V1\CommentRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 use App\Repositories\Api\V1\Product\ProductRepository;
 use App\Repositories\Api\V1\Product\ProductRepositoryInterface;
+use App\Repositories\Api\V1\Vote\VoteRepository;
+use App\Repositories\Api\V1\Vote\VoteRepositoryInterface;
 
 class RepositoryProvider extends ServiceProvider
 {
@@ -26,5 +30,7 @@ class RepositoryProvider extends ServiceProvider
     public function boot()
     {
         $this->app->bind(ProductRepositoryInterface::class, ProductRepository::class);
+        $this->app->bind(CommentRepositoryInterface::class, CommentRepository::class);
+        $this->app->bind(VoteRepositoryInterface::class, VoteRepository::class);
     }
 }
