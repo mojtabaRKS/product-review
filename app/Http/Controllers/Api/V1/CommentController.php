ac32a6443758da48a57de3e13d29863291e86c9e
mojtaba rakhisi <mojtaba.rakhisi@gamil.com>
2022-03-14 04:30:56 +0330

    refac: remove redundant return data

--- app/Http/Controllers/Api/V1/CommentController.php
+++ app/Http/Controllers/Api/V1/CommentController.php
@@ -61,9 +61,7 @@ class CommentController extends Controller
             $this->commentService->changeStatus($id, $request->validated());
             DB::commit();
             return $this->successResponse(
-                trans('messages.action_successfully_done'),
-                [],
-                Response::HTTP_CREATED
+                trans('messages.action_successfully_done')
             );
         } catch (Throwable $exception) {
             DB::rollBack();
