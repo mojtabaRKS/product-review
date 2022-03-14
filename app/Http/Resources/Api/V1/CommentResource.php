931779cf6e7fb5bf296bb2685acd3214f715dab7
mojtaba rakhisi <mojtaba.rakhisi@gamil.com>
2022-03-14 04:42:55 +0330

    fix : add id for comments

--- app/Http/Resources/Api/V1/CommentResource.php
+++ app/Http/Resources/Api/V1/CommentResource.php
@@ -15,6 +15,7 @@ class CommentResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'description' => $this->description,
             'user_id' => $this->user_id,
             'status' => $this->status,
